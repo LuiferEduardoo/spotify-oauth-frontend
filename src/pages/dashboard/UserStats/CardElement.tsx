9cf4abd88d4d654beb0ab35d0f1470cb67776641
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-03-04 19:34:03 -0500

    feat: add spotify logo and url for genres

--- src/pages/dashboard/UserStats/CardElement.tsx
+++ src/pages/dashboard/UserStats/CardElement.tsx
@@ -28,11 +28,19 @@ const CardElement: React.FC<propsCardElement> = ({
         )
       ) : (
         <a
-          href={item.url}
+          href={viewType === "genres" ? `https://open.spotify.com/search/${item.name}/genres` : item.url }
           target="_blank"
           rel="noopener noreferrer"
-          className="flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md"
+          className="relative flex flex-col items-center bg-gray-800 p-4 rounded-lg shadow-md"
         >
+          {/* Logo de Spotify en la esquina superior izquierda */}
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg"
+            alt="Spotify Logo"
+            className="absolute bg-gray-900 p-2 rounded-lg top-2 left-2 w-16"
+          />
+
+          {/* Imagen del álbum o track */}
           <img
             src={
               item.imageUrl ||
@@ -43,6 +51,8 @@ const CardElement: React.FC<propsCardElement> = ({
               item.imageUrl ? "rounded-full" : ""
             } object-cover mb-4`}
           />
+
+          {/* Información */}
           <h3 className="text-lg font-bold">{item.name}</h3>
           {viewType === "tracks" && (
             <p className="text-sm">
