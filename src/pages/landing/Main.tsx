543a189630560fe5caf1cf8c315a13fe4c289491
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2025-06-02 16:07:04 -0500

    feat: improve landing

--- src/pages/landing/Main.tsx
+++ src/pages/landing/Main.tsx
@@ -1,38 +1,37 @@
 const Main = () => {
   return (
     <main className="container mx-auto px-6 md:px-10 lg:px-20 my-20">
-      <section className="text-center mb-12 max-w-3xl mx-auto">
-        <h2 className="text-3xl font-semibold">
-          Discover Your Music Trends with BeatMap
-        </h2>
-        <p className="mt-4 text-gray-300">
-          Filter your stats by the last month, the past six months, or even the
-          last year. Gain insights into your listening habits and uncover
-          patterns in your musical journey.
-        </p>
+      <section className="mb-12 max-w-5xl mx-auto flex flex-col-reverse md:flex-row items-center text-center md:text-left gap-8">
+        <div className="flex-1">
+          <h1 className="text-3xl font-semibold">
+            Discover Your Music Trends with BeatMap
+          </h1>
+          <p className="mt-4 text-gray-300">
+            Filter your stats by the last month, the past six months, or even
+            the last year. Gain insights into your listening habits and uncover
+            patterns in your musical journey.
+          </p>
+        </div>
+        <img
+          src="https://beatmap.img.luifereduardoo.com/account.webp"
+          alt="BeatMap Account Overview"
+          className="w-full h-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl  mb-8 md:mb-0"
+        />
       </section>
 
-      <section className="flex flex-wrap gap-6 justify-center">
-        <div className="bg-gray-700 p-6 rounded-lg shadow-lg text-center w-full md:w-1/3">
-          <h3 className="text-xl font-semibold">Top Artists</h3>
-          <p className="mt-2 text-gray-400">
-            See which artists you've been vibing with the most.
-          </p>
-          <img src="https://beatmap.img.luifereduardoo.com/artist.webp" />
-        </div>
-        <div className="bg-gray-700 p-6 rounded-lg shadow-lg text-center w-full md:w-1/3">
-          <h3 className="text-xl font-semibold">Favorite Songs</h3>
-          <p className="mt-2 text-gray-400">
-            Rediscover the tracks you just can't stop playing.
-          </p>
-          <img src="https://beatmap.img.luifereduardoo.com/songs.webp" />
-        </div>
-        <div className="bg-gray-700 p-6 rounded-lg shadow-lg text-center w-full md:w-1/3">
-          <h3 className="text-xl font-semibold">Top Genres</h3>
-          <p className="mt-2 text-gray-400">
-            Explore the genres that define your music taste.
-          </p>
-          <img src="https://beatmap.img.luifereduardoo.com/gender.webp" />
+      <section className="mb-12 max-w-5xl mx-auto flex flex-col-reverse md:flex-row items-center text-center md:text-left gap-8">
+        <img
+          src="https://beatmap.img.luifereduardoo.com/trends.webp"
+          alt="trends"
+          className="w-full h-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w-2xl  mb-8 md:mb-0"
+        />
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold">Here you can...</h2>
+          <ul className="list-disc pl-5 space-y-2">
+            <li>See your favorite artists</li>
+            <li>See your most listened to genres</li>
+            <li>See your most listened to songs</li>
+          </ul>
         </div>
       </section>
     </main>
